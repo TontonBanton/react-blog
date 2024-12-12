974d9f32816df9b5ac42e32a8ade9834c675483e
TontonBanton <pancla13ulas@gmail.com>
2024-12-12 16:57:47 +0800

    Handlick Event onClick e

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log('simple button handler', e)
+  }
+  const handleClick2 = (name, e) => {
+    console.log('handler with arguments ', name, e)
+  }
+
   return (
     <div className="Home">
       <h2>Home Component</h2>
+      <button onClick={handleClick}>Click</button>
+      <button onClick={ (e) => handleClick2('myname', e) }>Click 2</button>
     </div>
   )
 }
